d074726270266bbb91ffa7d316d7ed918b921957
SvenPegels <sven.pegels@gmail.com>
2023-05-15 12:46:03 +0200

    Initial commit - pcl_test folder. Contains c++ pcl project to test out pcl::visualization

---
+++ pcl_test/build/CMakeFiles/pcd_write_test.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/svenp/rp-mass-and-com/pcl_test/pcd_write.cpp" "/home/svenp/rp-mass-and-com/pcl_test/build/CMakeFiles/pcd_write_test.dir/pcd_write.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_DATE_TIME_DYN_LINK"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_IOSTREAMS_DYN_LINK"
+  "BOOST_SERIALIZATION_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "DISABLE_LIBUSB"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DIY_NO_THREADS"
+  "FMT_SHARED"
+  "H5_BUILT_AS_DYNAMIC_LIB"
+  "VTK_HAS_OGGTHEORA_SUPPORT"
+  "kiss_fft_scalar=double"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/pcl-1.13"
+  "/usr/include/eigen3"
+  "/usr/local/include/vtk-9.2"
+  "/usr/local/include/vtk-9.2/vtkfreetype/include"
+  "/usr/local/include/vtk-9.2/vtkhdf5"
+  "/usr/local/include/vtk-9.2/vtkhdf5/src"
+  "/usr/local/include/vtk-9.2/vtkhdf5/hl/src"
+  "/usr/local/include/vtk-9.2/vtkogg/include"
+  "/usr/local/include/vtk-9.2/vtklibproj/src"
+  "/usr/local/include/vtk-9.2/vtknlohmannjson/include"
+  "/usr/local/include/vtk-9.2/vtklibxml2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
